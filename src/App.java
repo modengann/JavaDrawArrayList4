0246c81c6510b857e0cb2e00bff693b9721a3ae6
modengann <99417989+modengann@users.noreply.github.com>
2022-11-03 11:25:33 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,43 @@
+/** YouTube: https://youtu.be/eHfGc4jUUh0
+ *  Goal: Remove/delete rain once it hits the bottom of the screen
+ *  Tasks:
+ *  1. Copy method from previous task - start(), addRain(), rainFall()
+ *  2. Complete deleteRain method
+ *  3. Add deleteRain() method to while loop in start
+ * 
+ */
+
+
+import javadraw.*;
+import java.util.*;
+
+public class App extends Window{
+    ArrayList<Oval> rain;
+    
+    public static void main(String[] args) {
+        Window.open();
+    }
+    //Task 1: Copy this method from part 3
+    //Task 3: Add deleteRain() method to the while loop
+    public void start(){
+        
+    }
+
+    //Task 1: Copy this method from part 3
+    public void rainFall(){
+        
+    }
+
+    //Task 1: Copy this method from part 3
+    public void addRain(){
+        
+        
+
+    }
+
+    //Task 2: Complete this method to delete rain that is going off screen. Make sure to avoid the ConcurrentModificationException described in the YouTube video
+    public void deleteRain(){
+
+    }
+
+}
